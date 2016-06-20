7682b2e56da9f264726ecb9ded1b63f739ac2119
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2016-06-20 08:40:02 +0200

    Save

---
+++ angular-dart/README.md
@@ -0,0 +1,4 @@
+# Note 
+
+This example is based on the [Angular Dart Quickstart](https://github.com/angular-examples/quickstart).
+
---
+++ angular-dart/web/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/platform/browser.dart';
+
+import 'package:angular2_getting_started/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
