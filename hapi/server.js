4d7d13f7467e4979648d1b7838c8607eee9b180d
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2016-06-20 08:27:27 +0200

    Save

--- hapi/server.js
+++ hapi/server.js
@@ -5,8 +5,8 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({ 
-  host: 'localhost', 
-  port: 8000 
+  host: '0.0.0.0',
+  port: 8080
 });
 
 // Add the route
